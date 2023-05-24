e803feed46de91bb915a29953760338f628ad0fa
Kevin Barabash <kevinb@khanacademy.org>
2023-05-24 11:49:04 -0400

    add missing files

---
+++ src/convert/import-jest-globals.test.ts
@@ -0,0 +1,54 @@
+import { transform, stateBuilder } from "./utils/testing";
+
+describe("jest globals", () => {
+  it("should import a single global in _test.jsx files", async () => {
+    const src = `expect(true).toBe(true);`;
+
+    expect(
+      await transform(
+        src,
+        stateBuilder({ config: { filePath: "foo_test.jsx" } })
+      )
+    ).toMatchInlineSnapshot(`
+      "import {expect} from '@jest/globals';
+      expect(true).toBe(true);"
+    `);
+  });
+
+  it("should import multiple globals in _test.js files", async () => {
+    const src = `describe("foo", () => { expect(true).toBe(true); });`;
+
+    expect(
+      await transform(
+        src,
+        stateBuilder({ config: { filePath: "foo_test.js" } })
+      )
+    ).toMatchInlineSnapshot(`
+      "import {describe, expect} from '@jest/globals';
+      describe(\\"foo\\", () => { expect(true).toBe(true); });"
+    `);
+  });
+
+  it("should not import globals in non-test files", async () => {
+    const src = `expect(true).toBe(true);`;
+
+    expect(
+      await transform(src, stateBuilder({ config: { filePath: "foo.jsx" } }))
+    ).toMatchInlineSnapshot(`"expect(true).toBe(true);"`);
+  });
+
+  it("should not re-import globals in _test.js files that have already been imported", async () => {
+    const src = `import {describe, expect} from "@jest/globals";
+    describe("foo", () => { expect(true).toBe(true); });`;
+
+    expect(
+      await transform(
+        src,
+        stateBuilder({ config: { filePath: "foo_test.js" } })
+      )
+    ).toMatchInlineSnapshot(`
+      "import {describe, expect} from \\"@jest/globals\\";
+          describe(\\"foo\\", () => { expect(true).toBe(true); });"
+    `);
+  });
+});
---
+++ src/convert/import-jest-globals.ts
@@ -0,0 +1,72 @@
+import traverse from "@babel/traverse";
+import * as t from "@babel/types";
+import { TransformerInput } from "./transformer";
+
+/**
+ * Import jest globals from @jest/globals.
+ */
+export function importJestGlobals({ file }: TransformerInput) {
+  const state = {
+    usedGlobals: new Set<string>(),
+    importedGlobals: new Set<string>(),
+  };
+  const GLOBALS = [
+    "expect",
+    "it",
+    "test",
+    "describe",
+    "beforeEach",
+    "afterEach",
+    "beforeAll",
+    "afterAll",
+  ];
+  traverse(
+    file,
+    {
+      ImportSpecifier(path, state) {
+        const { node } = path;
+        const parent = path.parentPath.node;
+        if (
+          parent.type === "ImportDeclaration" &&
+          parent.source.value === "@jest/globals" &&
+          node.imported.type === "Identifier" &&
+          GLOBALS.includes(node.imported.name)
+        ) {
+          state.importedGlobals.add(node.imported.name);
+        }
+      },
+      CallExpression(path, state) {
+        const { node } = path;
+        if (
+          node.callee.type === "Identifier" &&
+          GLOBALS.includes(node.callee.name) &&
+          !state.importedGlobals.has(node.callee.name)
+        ) {
+          state.usedGlobals.add(node.callee.name);
+        }
+      },
+      Program: {
+        exit(path) {
+          if (state.usedGlobals.size === 0) {
+            return;
+          }
+
+          const specifiers = [];
+          for (const global of state.usedGlobals) {
+            specifiers.push(
+              t.importSpecifier(t.identifier(global), t.identifier(global))
+            );
+          }
+
+          const importDeclaration = t.importDeclaration(
+            specifiers,
+            t.stringLiteral("@jest/globals")
+          );
+          path.node.body = [importDeclaration, ...path.node.body];
+        },
+      },
+    },
+    undefined,
+    state
+  );
+}
