d3fa0add720790d037fc517ca9dc9ddcb91cd842
Kevin Barabash <kevinb@khanacademy.org>
2023-07-14 17:26:44 -0400

    add new files

---
+++ src/runner/fix-eslint-issues.test.ts
@@ -0,0 +1,36 @@
+import { fixEslintIssues } from "./fix-eslint-issues";
+
+describe("fixEslintIssues", () => {
+  it("should do something", () => {
+    expect(true).toBe(true);
+  });
+
+  it("should replace no-unused-var with @typescript-eslint/no-unused-vars", () => {
+    const input = `// @flow
+const a = 5; // eslint-disable-line no-unused-var
+// eslint-disable-next-line no-unused-var
+const b = 10;`;
+
+    const output = fixEslintIssues(input, "test.js", "test.ts");
+
+    expect(output).toMatchInlineSnapshot(`
+      "// @flow
+      const a = 5; // eslint-disable-line @typescript-eslint/no-unused-vars
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const b = 10;"
+    `);
+  });
+
+  it("should add eslint-disable static-service/require-stories if converting from .js to .tsx", () => {
+    const input = `// @flow
+console.log("hello, world!");`;
+
+    const output = fixEslintIssues(input, "test.js", "test.tsx");
+
+    expect(output).toMatchInlineSnapshot(`
+      "/* eslint-disable static-service/require-stories */
+      // @flow
+      console.log(\\"hello, world!\\");"
+    `);
+  });
+});
---
+++ src/runner/fix-eslint-issues.ts
@@ -0,0 +1,19 @@
+export function fixEslintIssues(
+  code: string,
+  filePath: string,
+  targetFilePath: string
+) {
+  let result = code.replace(
+    /no-unused-var/g,
+    "@typescript-eslint/no-unused-vars"
+  );
+
+  // If the file was a .js file then it probably didn't have any stories.
+  // If the new file is a .tsx then we'll be checking if it does have stories
+  // so we need to disable `static-service/require-stories` for this file.
+  if (filePath.endsWith(".js") && targetFilePath.endsWith(".tsx")) {
+    result = "/* eslint-disable static-service/require-stories */\n" + result;
+  }
+
+  return result;
+}
